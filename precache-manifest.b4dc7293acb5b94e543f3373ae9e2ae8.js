4899f9c566822b47cb99fd7ba7da8a9bd2adba5c
Sally Beckwith <sallypbeckwith@gmail.com>
2019-09-06 14:53:10 -0400

    Updates

---
+++ precache-manifest.b4dc7293acb5b94e543f3373ae9e2ae8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e8fc00902bb2669062b2c98d0b2811",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8b683ac493d8e1492a0d",
+    "url": "/monsters-rolodex/static/css/main.b47b46e1.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "8b683ac493d8e1492a0d",
+    "url": "/monsters-rolodex/static/js/main.6f972569.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
